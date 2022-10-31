0db29e86a82d6382d7fac771b13c056075b6d0eb
qwer083 <Qwertyiop890>
2022-10-31 20:37:04 +0300

    Initial commit

---
+++ src/test/java/Primitives.java
@@ -0,0 +1,14 @@
+public class Primitives {
+    public static void main (String [] args) {
+        byte a = -5;
+        short b = 32766;
+        int c = 2147483635;
+        long d = 3695005405L;
+        char e = 651;
+
+        float g = 0f;
+        double h = 3.14;
+
+        boolean i = true;
+    }
+}
---
+++ src/test/java/Wrappers.java
@@ -0,0 +1,19 @@
+public class Wrappers {
+    public static void main(String[] args) {
+        Byte a = -123;
+
+        Short b = 1256;
+
+        Integer c = 21474;
+
+        Long d = 48765L;
+
+        Character e = 'h';
+
+        Float g = 45.4565F;
+
+        Double h = 54.58;
+        
+        Boolean i = true;
+    }
+}
